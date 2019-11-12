54959f785e173f3133359ef179e5c476d6c0fd7b
retas-seven <retas_seven@hotmail.co.jp>
2019-11-12 20:23:54 +0900

    コード作成

---
+++ 3.3/app.js
@@ -0,0 +1,19 @@
+Vue.component('fruits-item-name', {
+    props: {
+        fruitsItem: {
+            type: Object,
+            required: true
+        }
+    },
+    template: '<li>{{fruitsItem.name}}</li>'
+})
+  
+new Vue({
+    el: '#fruits-component',
+    data: {
+        fruitsItems: [
+            {name: '梨'},
+            {name: 'イチゴ'}
+        ]
+    }
+})
