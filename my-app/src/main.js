7eef84a7fcca0423ea511b9537b3b0fc8a27a103
retas-seven <retas_seven@hotmail.co.jp>
2019-12-07 14:24:17 +0900

    Vuex導入〜動作確認

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import App from './App'
+import store from '@/store.js'
+
+// storeの情報を操作できることを確認
+console.log(store.state.count)
+store.commit('increment')
+console.log(store.state.count)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store, // storeを登録
   render: h => h(App)
 })
