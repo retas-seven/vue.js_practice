0002539c966574e1e70fa2b88049671c4f9d8190
retas-seven <retas_seven@hotmail.co.jp>
2020-01-08 19:11:58 +0900

    URLパラメータでコンポーネントを振り分ける処理を追加

--- my-app/src/views/Product.vue
+++ my-app/src/views/Product.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="product">
-    <h1>商品情報</h1>
+    <h1>商品詳細</h1>
+    <p>このページは ID.{{ id }}の詳細を表示する</p>
   </div>
 </template>
+
+<script>
+export default {
+  props: {id: Number}
+}
+</script>
---
+++ my-app/src/views/ProductList.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="product-list">
+    <h1>商品一覧</h1>
+    <p>URLパラメータでIDを指定しない場合は商品一覧を表示する</p>
+  </div>
+</template>
